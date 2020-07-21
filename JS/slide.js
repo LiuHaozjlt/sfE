55b8f5409f4e162bb0750867393a715689b5d42f
LiuHaozjlt <3178881449@qq.com>
2020-07-21 18:16:28 +0800

    7/21

--- JS/slide.js
+++ JS/slide.js
@@ -1,24 +1,32 @@
 $(function(){
 	
 	//导航菜单
-	$('div[topnav]').hover(function(){
-		var it = $(this).attr('attr');
-		if(it === 'undefined') return false;
-		var heht = $(this).find('#' + it).height() + 300 + "px";
-		
-		$(this).find('.submnu').height(heht);
-		
-		$(this).stop().find('.submnu').animate({height:'show'},{
-			queue:false,
-			duration:500,
-			easing:'easeOutBounce'
-		});
-		
-		$(this).stop().attr('class', 'imenu_on').siblings().attr('class', 'imenu');
-		
-	},function(){
-		$(this).stop().find('.submnu').hide();
-		$(this).stop().attr('class', 'imenu');
-	});  
+//	$('div[topnav]').hover(function(){
+//		console.log(2121212,$(this))
+//		var it = $(this).attr('attr');
+//		if(it === 'undefined') return false;
+//		var heht = $(this).find('#' + it).height() + 300 + "px";
+//		
+//		$(this).find('.submnu').height(heht);
+//		
+//		$(this).stop().find('.submnu').animate({height:'show'},{
+//			queue:false,
+//			duration:500,
+//			easing:'easeOutBounce'
+//		});
+//		
+//		$(this).stop().attr('class', 'imenu_on').siblings().attr('class', 'imenu');
+//		
+//	},function(){
+//		$(this).stop().find('.submnu').hide();
+//		$(this).stop().attr('class', 'imenu');
+//	});  
+
+//	$('.submnu')[0].show()
+$(".imenu a").click(function(){
+	$(this).addClass('active')
+	$('.submnu').hide()
+	$(this).parent().children('.submnu').slideToggle()
+})
 
 });
